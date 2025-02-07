94ed129067381845d9953f036e4bd47697bae50c
CrlThr <150603455+CrlThr@users.noreply.github.com>
2025-02-07 17:02:46 +0100

    [ADD] added color grading shader

--- Assets/Shaders/ColorGrading/Compute.hlsl
+++ Assets/Shaders/ColorGrading/Compute.hlsl
@@ -3,7 +3,22 @@
 // > Create Time: 2025-02-04 11:25:41
 //
 
+struct PushConstants {
+  int TextureIndex; 
+  float Brightness;   
+  float Exposure;
+};
+
+ConstantBuffer<PushConstants> Settings : register(b0);
+
 [numthreads(8, 8, 1)]
 void CSMain(uint3 ThreadID : SV_DispatchThreadID)
 {
+    RWTexture2D<float4> Texture = ResourceDescriptorHeap[Settings.TextureIndex]; 
+    Color = Texture[ThreadID.xy];
+
+    Color *= Settings.Brightness; 
+    Color = pow(Color, Settings.Exposure); 
+    Texture[ThreadID.xy] = Color;
+
 }
