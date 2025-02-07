372a411ea2ee5b9f88fc38a60fe833ffaf470266
Axel Moras <axel.moras4012@gmail.com>
2025-02-07 15:22:53 +0100

    [ADD] Box blur

--- Assets/Shaders/DOF/Compute.hlsl
+++ Assets/Shaders/DOF/Compute.hlsl
@@ -1,5 +1,45 @@
-[numthreads(8, 8, 1)]
-void CSMain(uint3 threadID : SV_DispatchThreadID)
+struct ToneMapperSettings
 {
-  
+    uint InputIndex;
+
+    uint3 Pad;
+};
+
+ConstantBuffer<ToneMapperSettings> Settings : register(b0);
+
+[numthreads(8, 8, 1)]
+void CSMain(uint3 ThreadID : SV_DispatchThreadID)
+{
+    RWTexture2D<float4> Output = ResourceDescriptorHeap[Settings.InputIndex];
+
+    uint Width, Height;
+    Output.GetDimensions(Width, Height); 
+
+    if (ThreadID.x < Width && ThreadID.y < Height)
+    {
+        int blurRadius = 3;
+
+        float4 colorAccumulator = float4(0, 0, 0, 0);
+
+        for (int dx = -blurRadius; dx <= blurRadius; dx++)
+        {
+            for (int dy = -blurRadius; dy <= blurRadius; dy++)
+            {
+
+                int2 neighborCoord = int2(ThreadID.x + dx, ThreadID.y + dy);
+
+                if (neighborCoord.x >= 0 && neighborCoord.x < Width && neighborCoord.y >= 0 && neighborCoord.y < Height)
+                {
+                    float4 neighborColor = Output[neighborCoord];
+
+                    colorAccumulator += neighborColor;
+                }
+            }
+        }
+
+        float numSamples = (blurRadius * 2 + 1) * (blurRadius * 2+1);
+        colorAccumulator /= numSamples;
+
+        Output[ThreadID.xy] = colorAccumulator;
+    }
 }
--- Engine/Mnemen/Renderer/Passes/DOF.cpp
+++ Engine/Mnemen/Renderer/Passes/DOF.cpp
@@ -5,15 +5,35 @@
 
 #include "DOF.hpp"
 
+
 DOF::DOF(RHI::Ref rhi)
     : RenderPass(rhi)
 {
-    // Axel
+    Asset::Handle computeShader  = AssetManager::Get("Assets/Shaders/DOF/Compute.hlsl", AssetType::Shader);
+
+    auto signature = mRHI->CreateRootSignature({ RootType::PushConstant}, sizeof(int) * 4);
+    mPipeline = mRHI->CreateComputePipeline(computeShader->Shader, signature);
 }
 
 void DOF::Render(const Frame& frame, ::Ref<Scene> scene)
 {
-    // Axel
+    auto color = RendererTools::Get("HDRColorBuffer");
+
+    struct {
+        int Output;
+        glm::vec3 Pad;
+    } PushConstants = {
+        color->Descriptor(ViewType::Storage),
+        glm::vec3(0)
+    };
+
+    frame.CommandBuffer->BeginMarker("Box Blur");
+    frame.CommandBuffer->Barrier(color->Texture, ResourceLayout::Storage);
+    frame.CommandBuffer->SetComputePipeline(mPipeline);
+    frame.CommandBuffer->ComputePushConstants(&PushConstants, sizeof(PushConstants), 0);
+    frame.CommandBuffer->Dispatch(frame.Width / 8, frame.Height / 8, 1);
+    frame.CommandBuffer->Barrier(color ->Texture, ResourceLayout::Common);
+    frame.CommandBuffer->EndMarker();
 }
 
 void DOF::UI(const Frame& frame)
