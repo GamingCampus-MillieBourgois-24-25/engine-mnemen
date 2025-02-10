ce9d5697bfd1c3e9af54b7e7be5e1625aedd2dc7
TheJokr13 <150007860+TheJokr13@users.noreply.github.com>
2025-02-10 12:28:36 +0100

    [ADD] added textures and noise map into SSAO.cpp

--- Assets/Shaders/SSAO/Compute.hlsl
+++ Assets/Shaders/SSAO/Compute.hlsl
@@ -1,3 +1,12 @@
+float4 GetViewFromDepth(float2 uv, float depth, column_major float4x4 inverseProj)
+{
+    float4 result = float4(float3(uv * 2. - 1., depth), 1.);
+    result = mul(inverseProj, result);
+    result.xyz /= result.w;
+    result.y *= -1.0;
+    return result;
+}
+
 [numthreads(8, 8, 1)]
 void CSMain(uint3 threadID : SV_DispatchThreadID)
 {
