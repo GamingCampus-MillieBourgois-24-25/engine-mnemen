078b211c8f875137d983aa1ac280010ce76044b3
CrlThr <150603455+CrlThr@users.noreply.github.com>
2025-02-10 10:53:07 +0100

    [ADD] added saturation and contrast

--- Assets/Shaders/ColorGrading/Compute.hlsl
+++ Assets/Shaders/ColorGrading/Compute.hlsl
@@ -3,14 +3,37 @@
 // > Create Time: 2025-02-04 11:25:41
 //
 
+#include "Assets/Shaders/Common/Color.hlsl"
+
+#define ACEScc_MIDGRAY 0.4135884
+
 struct PushConstants {
+  // float4
   int TextureIndex; 
   float Brightness;   
   float Exposure;
   float Pad;
+
+  // float4
+  float Contrast;
+  float Saturation;
+  float2 Pad1;
 };
 
 ConstantBuffer<PushConstants> Settings : register(b0);
+
+float3 ColorGradingContrast(float3 color)
+{
+    color = LinearToLogC(color);
+    color = (color - ACEScc_MIDGRAY) * (Settings.Contrast * 0.1 + 1.0) + ACEScc_MIDGRAY;
+    return LogCToLinear(color);
+}
+
+float3 ColorGradingSaturation(float3 color)
+{
+    float luminance = Luminance(color);
+    return (color - luminance) * (Settings.Saturation * 0.1 + 1.0) + luminance;
+}
 
 [numthreads(8, 8, 1)]
 void CSMain(uint3 ThreadID : SV_DispatchThreadID)
@@ -21,9 +44,16 @@ void CSMain(uint3 ThreadID : SV_DispatchThreadID)
     Texture.GetDimensions(width, height);
 
     if (ThreadID.x < width && ThreadID.y < height) {
-        float4 Color = Texture[ThreadID.xy];
-        Color *= Settings.Brightness; 
-        Color = pow(Color, 1.0 / Settings.Exposure); 
-        Texture[ThreadID.xy] = Color;
+        
+        float3 BaseColor = Texture[ThreadID.xy].rgb;
+        
+        BaseColor = min(BaseColor, 60.0);
+        BaseColor = pow(BaseColor, 1.0 / Settings.Exposure);
+        BaseColor = ColorGradingContrast(BaseColor);
+        BaseColor *= Settings.Brightness; 
+        BaseColor = ColorGradingSaturation(BaseColor);
+        BaseColor = max(BaseColor, 0.0);
+
+        Texture[ThreadID.xy] = float4(BaseColor, 1.0);
     }
 }
