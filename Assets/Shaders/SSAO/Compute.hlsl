a2600d68f26d91d59ec10bcadabb12ee1ca99cee
TheJokr13 <150007860+TheJokr13@users.noreply.github.com>
2025-02-17 09:53:36 +0100

    [EDITED] broke shader while trying to make it work

--- Assets/Shaders/SSAO/Compute.hlsl
+++ Assets/Shaders/SSAO/Compute.hlsl
@@ -1,3 +1,13 @@
+// Input settings passed from the CPU to the shader
+struct PushConstants
+{
+    // Index of the input resource (HDR texture)
+    uint InputIndex;
+    uint3 Padding;
+};
+
+ConstantBuffer<PushConstants> Settings : register(b0);
+
 float4 GetViewFromDepth(float2 uv, float depth, column_major float4x4 inverseProj)
 {
     float4 result = float4(float3(uv * 2. - 1., depth), 1.);
@@ -7,8 +17,17 @@ float4 GetViewFromDepth(float2 uv, float depth, column_major float4x4 inversePro
     return result;
 }
 
+float rand1(float n)  { return frac(sin(n) * 43758.5453123); }
+
 [numthreads(8, 8, 1)]
-void CSMain(uint3 threadID : SV_DispatchThreadID)
+void CSMain(uint3 ThreadID : SV_DispatchThreadID)
 {
-  
+    RWTexture2D<float4> Output = ResourceDescriptorHeap[Settings.InputIndex]; 
+
+    uint Width, Height;
+    Output.GetDimensions(Width, Height);
+
+    if (ThreadID.x < Width && ThreadID.y < Height) {
+        Output[ThreadID.xy] = Output[ThreadID.xy];
+    }
 }
--- Engine/Mnemen/Renderer/Passes/SSAO.cpp
+++ Engine/Mnemen/Renderer/Passes/SSAO.cpp
@@ -7,6 +7,7 @@
 #include "RHI/Uploader.hpp"
 #include <Core/Application.hpp>
 #include <random>
+#include <imgui.h>
 
 float lerp(float a, float b, float f)
 {
@@ -19,71 +20,29 @@ SSAO::SSAO(RHI::Ref rhi)
     int width, height;
     Application::Get()->GetWindow()->PollSize(width, height);
 
-    std::uniform_real_distribution<float> randomFloats(0.0, 1.0); // random floats between [0.0, 1.0]
-    std::default_random_engine generator;
-    std::vector<glm::vec3> ssaoKernel;
-    for (unsigned int i = 0; i < 64; ++i)
-    {
-        glm::vec3 sample(
-            randomFloats(generator) * 2.0 - 1.0, 
-            randomFloats(generator) * 2.0 - 1.0, 
-            randomFloats(generator)
-        );
-        float scale = (float)i / 64.0; 
-        scale   = lerp(0.1f, 1.0f, scale * scale);
-        sample *= scale;
-        ssaoKernel.push_back(sample);  
-    }
-
-    std::vector<glm::vec3> ssaoNoise;
-    for (unsigned int i = 0; i < 16; i++)
-    {
-        glm::vec3 noise(
-            randomFloats(generator) * 2.0 - 1.0, 
-            randomFloats(generator) * 2.0 - 1.0, 
-            0.0f); 
-        ssaoNoise.push_back(noise);
-    }
-    
-    std::vector<UInt8> bytes;
-    bytes.resize(ssaoNoise.size() * sizeof(glm::vec3));
-    memcpy(bytes.data(), ssaoNoise.data(), bytes.size());
-
-    TextureDesc desc = {};
-    desc.Width = 4;
-    desc.Height = 4;
-    desc.Levels = 1;
-    desc.Depth = 1;
-    desc.Name = "Noise_Texture";
-    desc.Usage = TextureUsage::RenderTarget | TextureUsage::Storage | TextureUsage::ShaderResource;
-    desc.Format = TextureFormat::RGBA16Float;
-    
-    auto ldr = RendererTools::CreateSharedTexture("Noise_Texture", desc);
-    ldr->AddView(ViewType::Storage);
-    ldr->AddView(ViewType::ShaderResource);
-
-    Uploader::EnqueueTextureUpload(bytes, ldr->Texture);
-
     TextureDesc text2 = {};
     text2.Width = width;
     text2.Height = height;   
-    text2.Levels = 0;
+    text2.Levels = 1;
     text2.Depth = 1;
     text2.Name = "ssaoColorBuffer";
     text2.Usage = TextureUsage::RenderTarget | TextureUsage::Storage | TextureUsage::ShaderResource;
     text2.Format = TextureFormat::R32Float;
     
-    auto scb = RendererTools::CreateSharedTexture("ssaoColorBuffer", desc);
+    auto scb = RendererTools::CreateSharedTexture("ssaoColorBuffer", text2);
     scb->AddView(ViewType::Storage);
     scb->AddView(ViewType::ShaderResource);
 }
 
-void SSAO::Render(const Frame& frame, ::Ref<Scene> scene)
-{
-    // Alex
+void SSAO::Render(const Frame& frame, ::Ref<Scene> scene) {
+    
 }
 
 void SSAO::UI(const Frame& frame)
 {
     // Alex
+
+    if (ImGui::TreeNodeEx("SSAO", ImGuiTreeNodeFlags_Framed)) {
+        ImGui::TreePop();
+    }  
 }
